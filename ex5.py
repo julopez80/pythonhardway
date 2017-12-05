159996172a352c4541951f31343aed92963a1d4a
Juan F. Lopez <juanflopez80@gmail.com>
2017-12-04 23:11:14 -0300

    Ex5

--- ex5.py
+++ ex5.py
@@ -4,7 +4,7 @@ my_height = 74 # inches
 my_weight = 180 # lbs
 my_eyes = 'Blue'
 my_teeth = 'White'
-my_hair = 'Brown'
+my_hair = 'Black'
 
 print(f"Let's talk about {my_name}")
 print(f"He's {my_height} inches tall.")
